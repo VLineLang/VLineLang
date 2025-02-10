1382c800c1bf2f9f43db4bccc99ab4020fd5dfbb
karsl-program <2264284870@qq.com>
2025-02-10 11:14:19 +0800

    fix(version): fix version for fix issue #2

--- utils/core.hpp
+++ utils/core.hpp
@@ -1,7 +1,7 @@
 #ifndef CORE_HPP
 #define CORE_HPP
 
-#define VLINE_VERSION "0.8.5-alpha.2"
+#define VLINE_VERSION "0.8.6-alpha.1"
 #define VLINE_PUBLISH "Feb. 6th, 2025"
 #define VLINE_COMPILER "GNU GCC/ISO C++17"
 
